e91a434f5d51d31298612c02a4e274e4cfb97620
Sufiyan0007 <sufiyanlaxmeshwar71@gmail.com>
2025-05-07 16:30:14 +0530

    save

---
+++ DivisionOperation.java
@@ -0,0 +1,16 @@
+public class DivisionOperation {
+    public static void main(String[] args) {
+        int a= 15;
+        int b =0;
+        try{
+            int div =a/b;
+            System.out.println("result:"+ div);
+        }
+        catch
+        (ArithmeticException e){
+            System.out.println("error:Division by zero is not allowed");
+        }
+        
+    }
+
+}
